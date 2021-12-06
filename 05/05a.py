4591e5687ed5e5d8295605a0a279ce50d93d9609
seibaek <95361607+seibaek@users.noreply.github.com>
2021-12-06 23:43:49 +1100

    updated to use sets

--- 05/05a.py
+++ 05/05a.py
@@ -1,14 +1,11 @@
-# counter = 0
-
 def create_list():
-    fifth_input = open("input05", "r")
+    fifth_input = open("input05.txt", "r")
     new_list = fifth_input.read().replace("\n\n", "\n").split("\n")
     fifth_input.close()
     return new_list
 
 def process_points(input_list):
     new_list = list(filter(filter_blanks, input_list))
-    # print(len(new_list))
     atob = []
     for i, value in enumerate(new_list):
         line = value.split(' -> ')
@@ -16,7 +13,6 @@ def process_points(input_list):
         for y in line:
             xandy.append(y.split(','))
         atob.append(xandy)
-    # print(atob)
     return solution_five(atob)
 
 def filter_blanks(input_list):
@@ -26,44 +22,38 @@ def filter_blanks(input_list):
         return True
 
 def solution_five(input_list):
-    count_list = []
+    count_list = set()
     solution_list = set()
-    counter = 0
     for h, hv in enumerate(input_list):
-        # print(hv[0][1])
-        # print(hv[1][1])
-        count_list.append(hv[0])
-        count_list.append(hv[1])
         x1 = hv[0][0]
         x2 = hv[1][0]
         y1 = hv[0][1]
         y2 = hv[1][1]
         if abs(int(y1) - int(y2)) > 0:
-        #     # print(abs(int(y1) - int(y2)))
-            for j in range(abs(int(y1) - int(y2)) - 1):
+            for j in range(abs(int(y1) - int(y2))):
                 if int(y1) < int(y2):
-                    if count_list.count([y2, str(int(x2) + j + 1)]) >= 1:
-                        solution_list.add(str([x1, str(int(y1) + j + 1)]))
+                    if str([x1, str(int(y1) + j)]) not in count_list:
+                        count_list.add(str([x1, str(int(y1) + j)]))
                     else:
-                        count_list.append([x1, str(int(y1) + j + 1)])
-                else:
-                    if count_list.count([str(int(y2) + j + 1), x2]) >= 1:
-                        solution_list.add(str([x2, str(int(y2) + j + 1)]))
+                        solution_list.add(str([x1, str(int(y1) + j)]))
+                elif int(y1) > int(y2):
+                    if str([x2, str(int(y2) + j)]) not in count_list:
+                        count_list.add(str([x2, str(int(y2) + j)]))
                     else:
-                        count_list.append([x2, str(int(y2) + j + 1)])
+                        solution_list.add(str([x2, str(int(y2) + j)]))
         if abs(int(x1) - int(x2)) > 0:
-            for j in range(abs(int(x1) - int(x2)) - 1):
+            for j in range(abs(int(x1) - int(x2))):
                 if int(x1) < int(x2):
-                    if count_list.count([y1, str(int(x1) + j + 1)]) >= 1:
-                        solution_list.add(str([y1, str(int(x1) + j + 1)]))
+                    if str([str(int(x1) + j), y1]) not in count_list:
+                        count_list.add(str([str(int(x1) + j), y1]))
                     else:
-                        count_list.append([y1, str(int(x1) + j + 1)])
-                else:
-                    if count_list.count([y2, str(int(x2) + j + 1)]) >= 1:
-                        solution_list.add(str([y2, str(int(x2) + j + 1)]))
+                        solution_list.add(str([str(int(x1) + j), y1]))
+                elif int(x1) > int(x2):
+                    if str([str(int(x2) + j), y2]) not in count_list:
+                        count_list.add(str([str(int(x2) + j), y2]))
                     else:
-                        count_list.append([y2, str(int(x2) + j + 1)])
+                        solution_list.add(str([str(int(x2) + j), y2]))
     print(len(solution_list))
 
-process_points(create_list())
 
+process_points(create_list())
